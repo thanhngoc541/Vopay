2efb3e447a62ee2cfae45aebf761e7579374a76a
ngoc <ngochc1@gmail.com>
2023-12-26 17:24:03 +0700

    vopay API

---
+++ src/app.controller.ts
@@ -0,0 +1,44 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService,
+    private readonly httpService: HttpService) { }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get("signature")
+  getSignature() {
+    return { signature: this.appService.getSignature() };
+  }
+
+  @Get("auth/ping")
+  authPing() {
+    return this.appService.authPing();
+  }
+
+  @Get("account/balance")
+  getAccountBalance() {
+    return this.appService.getAccountBalance();
+  }
+
+  @Post("settings/setup-sender-email")
+  setEmailSender(@Body() body: any) {
+    return this.appService.setSenderEmail(body)
+  }
+
+  @Post("paylink")
+  createPaylink(@Body() body: any) {
+    return this.appService.createPayLink(body);
+  }
+  @Post("account/transactions")
+  getAccountTransactions(@Body() body: any) {
+    return this.appService.getAccountTransactions(body);
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,109 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { AxiosError } from 'axios';
+import { firstValueFrom, catchError } from 'rxjs';
+
+@Injectable()
+export class AppService {
+  constructor(private readonly httpService: HttpService) { }
+  getHello(): string {
+    return 'Hello World!';
+  }
+  getSignature() {
+    var crypto = require('crypto')
+    var shasum = crypto.createHash('sha1')
+
+    var key = process.env.API_KEY
+    var secret = process.env.SECRET
+    let date = new Date();
+
+    var dateString = date.toISOString().split('T')[0]
+
+    shasum.update(key + secret + dateString)
+    var signature = shasum.digest('hex')
+    console.log("signature: ", signature);
+    return signature;
+  }
+  async authPing() {
+    const form = new FormData();
+    form.append('AccountID', process.env.ACCOUNT_ID);
+    form.append('Key', process.env.API_KEY);
+    form.append('Signature', this.getSignature());
+    const { data } = await firstValueFrom(
+      this.httpService
+        .post('https://earthnode-dev.vopay.com/api/v2/auth/ping',
+          form
+        ).pipe(
+          catchError((error: AxiosError) => {
+            throw 'An error happened!';
+          }),
+        ),
+    );
+    return data;
+  }
+  async getAccountBalance() {
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get(`https://earthnode-dev.vopay.com/api/v2/account/balance?AccountID=${process.env.ACCOUNT_ID}&Key=${process.env.API_KEY}&Signature=${this.getSignature()}`
+        ).pipe(
+          catchError((error: AxiosError) => {
+            throw 'An error happened!';
+          }),
+        ),
+    );
+    return data;
+  }
+
+  async getAccountTransactions(body) {
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get(`https://earthnode-dev.vopay.com/api/v2/account/transactions?AccountID=${process.env.ACCOUNT_ID}&Key=${process.env.API_KEY}&Signature=${this.getSignature()}&StartDateTime=${body.startDate}&EndDateTime=${body.endDate}`
+        ).pipe(
+          catchError((error: AxiosError) => {
+            throw 'An error happened!';
+          }),
+        ),
+    );
+    return data;
+  }
+  async setSenderEmail(body) {
+    const form = new FormData();
+    form.append('AccountID', process.env.ACCOUNT_ID);
+    form.append('Key', process.env.API_KEY);
+    form.append('Signature', this.getSignature());
+    form.append('SenderEmailAddress', body.email);
+    const { data } = await firstValueFrom(
+      this.httpService
+        .post('https://earthnode-dev.vopay.com/api/v2/paylink/settings/setup-sender-email',
+          form
+        ).pipe(
+          catchError((error: AxiosError) => {
+            throw 'An error happened!';
+          }),
+        ),
+    );
+    return data;
+  }
+  async createPayLink(body) {
+    const form = new FormData();
+    form.append('AccountID', process.env.ACCOUNT_ID);
+    form.append('Key', process.env.API_KEY);
+    form.append('Signature', this.getSignature());
+    form.append('SenderName', body.senderName);
+    form.append('ReceiverName', body.receiverName);
+    form.append('PaymentType', body.paymentType);
+    form.append('Amount', body.amount);
+    form.append('ReceiverEmailAddress', body.receiverEmailAddress);
+    const { data } = await firstValueFrom(
+      this.httpService
+        .post('https://earthnode-dev.vopay.com/api/v2/paylink',
+          form
+        ).pipe(
+          catchError((error: AxiosError) => {
+            throw 'An error happened!';
+          }),
+        ),
+    );
+    return data;
+  }
+}
